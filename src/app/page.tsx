9ee16b26ca9edbc7f00b3c1ff1eaa719c774c856
dev-ahmedali <ahmedali69325@gmail.com>
2023-08-14 16:01:25 +0600

    updated home pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import {prisma} from "@/lib/db"
+import SigninButton from "@/components/SigninButton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <Button>Hello World</Button>
-  )
+    <div className="absolute -translate-x-1/2 left-1/2 -translate-y-1/2 top-1/2">
+      <Card className="w-[300px]">
+        <CardHeader>
+          <CardTitle>
+            Welcome to Quysy!
+            </CardTitle>
+            <CardDescription>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni
+              veniam expedita quaerat quibusdam facere libero?
+            </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SigninButton text="Sign In with Google"/>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
